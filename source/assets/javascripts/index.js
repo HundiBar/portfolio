57bb03d9fc65328815709c587d041b670edea1bd
Dean Abbott <deanabbott293@gmail.com>
2021-09-17 13:54:43 +0900

    added js hambyrger nav

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,1 +1,7 @@
 import 'bootstrap';
+
+const navOpen = document.querySelector('.jedi-burger');
+navOpen.addEventListener('click', () => {
+  console.log(navOpen);
+  document.querySelector('.nav-links').classList.toggle('open');
+});
