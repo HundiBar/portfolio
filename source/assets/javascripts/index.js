27dd0a8c8203674717ca397a8ee17fccd0cd18a1
Dean Abbott <deanabbott293@gmail.com>
2021-09-19 16:36:28 +0900

    restyling portolio

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,7 +1,8 @@
 import 'bootstrap';
 
-const navOpen = document.querySelector('.jedi-burger');
-navOpen.addEventListener('click', () => {
+const navOpen = document.querySelector('.hamburger');
+navOpen.addEventListener('click', (event) => {
   console.log(navOpen);
   document.querySelector('.nav-links').classList.toggle('open');
+  navOpen.classList.toggle('open');
 });
