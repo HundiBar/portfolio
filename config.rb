8cbcadde31fea740d680d9398aa6193892f04dab
Dean Abbott <deanabbott293@gmail.com>
2021-09-25 16:10:33 +0900

    meta_tags setup

--- config.rb
+++ config.rb
@@ -19,6 +19,8 @@ activate :autoprefixer do |config|
   config.browsers = '>0.25%, not ie 11, not op_mini all, not dead'
 end
 
+activate :metaman, host: 'https://domain.com'
+
 activate :external_pipeline,
          name: :webpack,
          command: build? ? 'npm run build' : 'npm run start',
